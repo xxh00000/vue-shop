6cb08d78a037d36c0ab3dce444b7c06fa1e47622
xxh00000 <1578575212@qq.com>
2020-12-20 21:36:46 +0800

    完成用户登录功能

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,20 +1,26 @@
 <!--
  * @Author: your name
  * @Date: 2020-12-14 14:32:49
- * @LastEditTime: 2020-12-16 13:29:22
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2020-12-20 21:20:33
+ * @LastEditors: Wang Jin
  * @Description: In User Settings Edit
  * @FilePath: \vue-shop\src\views\login.vue
 -->
 <template>
   <div class="login-container">
     <div class="login-left">
-      <img src="../assets/login_img.png" alt="无法显示图片">
+      <img
+        src="../assets/login_img.png"
+        alt="无法显示图片"
+      >
     </div>
     <div class="login-box">
       <!-- 头像 -->
       <div class="login-logo">
-        <img src="../assets/login_logo.png" alt="logo">
+        <img
+          src="../assets/login_logo.png"
+          alt="logo"
+        >
       </div>
       <!-- 标题 -->
       <div class="login-title">
@@ -22,14 +28,32 @@
       </div>
       <!-- input表单 -->
       <div class="login-form">
-        <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" status-icon>
+        <el-form
+          ref="loginFormRef"
+          :model="loginForm"
+          :rules="loginFormRules"
+          status-icon
+        >
           <el-form-item prop="username">
-             <el-input v-model="loginForm.username"  placeholder="请输入用户名" prefix-icon=" iconfont icon-user" ></el-input>
+            <el-input
+              v-model="loginForm.username"
+              placeholder="请输入用户名"
+              prefix-icon=" iconfont icon-user"
+            ></el-input>
           </el-form-item>
           <el-form-item prop="password">
-             <el-input v-model="loginForm.password"  placeholder="请输入密码" prefix-icon=" iconfont icon-3702mima"  type="password"></el-input>
+            <el-input
+              v-model="loginForm.password"
+              placeholder="请输入密码"
+              prefix-icon=" iconfont icon-3702mima"
+              type="password"
+            ></el-input>
           </el-form-item>
-          <el-button type="primary" class="login-butn" @click="login">登录</el-button>
+          <el-button
+            type="primary"
+            class="login-butn"
+            @click="login"
+          >登录</el-button>
         </el-form>
       </div>
     </div>
@@ -37,49 +61,54 @@
 </template>
 
 <script>
-  export default {
-    data(){
-      return{
-        //表单的数据绑定对象
-        loginForm:{
-            username: 'admin',
-            password: '123456'
-        },
-        //表单的校验规则,以对象形式
-        loginFormRules:{
-          username:[
-              {required: true, message:'请输入登录账户',trigger: 'blur'},
-              {min: 3,max: 10,message:'长度在3到10个字符',trigger: 'blur'}
-          ],
-           passward:[
-              {required: true, message:'请输入密码', trigger: 'blur'},
-              {min: 6,max: 10, message:'长度在6到10个字符', trigger: 'blur'}
-          ]
+export default {
+  data() {
+    return {
+      //表单的数据绑定对象
+      loginForm: {
+        username: "admin",
+        password: "123456",
+      },
+      //表单的校验规则,以对象形式
+      loginFormRules: {
+        username: [
+          { required: true, message: "请输入登录账户", trigger: "blur" },
+          { min: 3, max: 10, message: "长度在3到10个字符", trigger: "blur" },
+        ],
+        passward: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { min: 6, max: 10, message: "长度在6到10个字符", trigger: "blur" },
+        ],
+      },
+    };
+  },
+  methods: {
+    login() {
+      this.$refs.loginFormRef.validate(async (valid) => {
+        if (!valid) {
+          return;
         }
-      }
+        //发送请求是否登陆成功
+        const { data: res } = await this.$http.post("login", this.loginForm);
+        //登录判断
+        console.log(res);
+        if (res.meta.status !== 200) {
+          return this.$message.error("登录失败");
+        }
+        this.$message.success("登录成功");
+        //路由跳转
+        this.$router.push("/home");
+        //将用户信息保存到sessionStorage中注意sessionStorage只能存储string对象
+        sessionStorage.setItem("userinfo", JSON.stringify(res.data));
+        console.log(res);
+      });
     },
-    methods:{
-      login(){
-       this.$refs.loginFormRef.validate(async valid =>{
-         if(!valid){
-           return
-         }
-            //发送请求是否登陆成功
-             const res= await this.$http.post('login',this.loginForm)
-           
-             console.log(res)
-           
-             
-       })
-      
-      }
-    }
-    
-  }
+  },
+};
 </script>
 
 <style>
-.login-container{
+.login-container {
   height: 100%;
   background-color: #93defe;
   /* 实现元素的水平垂直居中 */
@@ -87,17 +116,17 @@
   justify-content: center;
   align-items: center;
 }
-.login-left{
-  margin-right: 100px
+.login-left {
+  margin-right: 100px;
 }
-.login-box{
- height: 350px;
- width: 400px;
- background-color: white;
- border-radius: 6px;
- position: relative;
+.login-box {
+  height: 350px;
+  width: 400px;
+  background-color: white;
+  border-radius: 6px;
+  position: relative;
 }
-.login-logo{
+.login-logo {
   border: 4px #93defe solid;
   width: 120px;
   height: 120px;
@@ -108,18 +137,18 @@
   background-color: white;
   right: 140px;
 }
-.login-logo img{
+.login-logo img {
   width: 70px;
   height: 70px;
 }
-.login-title{
+.login-title {
   font-size: 20px;
   color: #444444;
-  position:absolute;
+  position: absolute;
   top: 80px;
   left: 115px;
 }
-.login-form{
+.login-form {
   position: absolute;
   bottom: 0;
   left: 50%;
@@ -127,10 +156,8 @@
   padding: 10% 20%;
   box-sizing: border-box;
   width: 400px;
-  
-
 }
-.login-butn{
+.login-butn {
   width: 100%;
 }
 </style>
